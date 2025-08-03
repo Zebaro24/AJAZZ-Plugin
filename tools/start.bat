569e5c32fa0f242c61008ec3088b32c9f22474ec
Zebaro <denok100100@gmail.com>
2025-08-03 13:41:32 +0200

    Added auto-restart of the plugin in debug mode

--- tools/start.bat
+++ tools/start.bat
@@ -1,4 +1,8 @@
+@chcp 65001 >nul
 @echo off
-
+:loop
 set /p args=<C:\Users\Zebaro\AppData\Roaming\HotSpot\StreamDock\plugins\dev.zebaro.testVersion.sdPlugin\plugin\arguments.txt
 .\.venv\Scripts\python.exe .\main.py %args%
+echo Перезапуск через 2 секунды...
+timeout /t 2 /nobreak >nul
+goto loop
